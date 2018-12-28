891d14e5c711147cf78b5f77bec615e25b182dbf
Simon St. Barth <data5tream@protonmail.com>
2018-12-28 21:48:20 +0100

    Initial commit

---
+++ pwa/ts/app.ts
@@ -0,0 +1,2 @@
+// Register ServiceWorker
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/web_stuff/pwa/dist/js/sw.js');
---
+++ pwa/ts/sw.ts
@@ -0,0 +1,33 @@
+interface Event {
+    waitUntil;
+    respondWith;
+    request;
+}
+
+let cname: string = 'webStuffPWA-v1';
+let cfiles: Array<string> = [
+    '/web_stuff/pwa/',
+    '/web_stuff/pwa/index.html',
+    '/web_stuff/pwa/dist/js/app.min.js',
+    '/web_stuff/pwa/dist/css/style.min.css'
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(caches.open(cname).then(function(cache) {
+        return cache.addAll(cfiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(caches.match(e.request).then(function(r) {
+        console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then(function(response) {
+            return caches.open(cname).then(function(cache) {
+                console.log('[Service Worker] Caching new resource: '+e.request.url);
+                cache.put(e.request, response.clone());
+                return response;
+            });
+        });
+    }));
+});
