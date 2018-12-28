49e710af2a081751b9bb4b00c5f2e68ee0daca3f
Simon St. Barth <data5tream@protonmail.com>
2018-12-28 22:21:01 +0100

    Notification test

--- pwa/ts/app.ts
+++ pwa/ts/app.ts
@@ -1,2 +1,18 @@
 // Register ServiceWorker
 if('serviceWorker' in navigator) navigator.serviceWorker.register('/web_stuff/pwa/dist/js/sw.min.js');
+
+var button = document.getElementById('notification');
+button.addEventListener('click', function(e) {
+    Notification.requestPermission().then(function(result) {
+        if(result === 'granted') {
+            let nottitle: string = 'Notification';
+            let notbody: string = 'Test notification';
+            let noticon: string = '/web_stuff/pwa/dist/img/logo.png';
+            let options = {
+                body: notbody,
+                icon: noticon
+            }
+            let notification: Notification = new Notification(nottitle, options);
+        }
+    });
+});
